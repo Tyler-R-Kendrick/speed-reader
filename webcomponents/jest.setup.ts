925c41335fe0f4391460eb718b6137d9d399525a
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-06-29 06:50:58 -0500

    feat: adopt Spectrum components (#41)

--- webcomponents/jest.setup.ts
+++ webcomponents/jest.setup.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom';
+
+class DummyObserver {
+  observe() {
+    // no-op for tests
+  }
+  unobserve() {}
+  disconnect() {}
+  takeRecords() {
+    return [];
+  }
+}
+
+// Polyfill observers used by Spectrum
+if (!('ResizeObserver' in global)) {
+  (global as any).ResizeObserver = DummyObserver;
+}
+if (!('IntersectionObserver' in global)) {
+  (global as any).IntersectionObserver = DummyObserver;
+}
